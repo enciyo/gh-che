84ac9d092cb108cce8272ef15dc3cae827f4588e
enciyo <mustafa.kilic@ciceksepeti.com>
2024-06-07 19:10:13 +0300

    Fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gh-che',
-    version='3.0.2',
+    version='1.0.4',
     packages=['che', 'che.intercept'],
     license='LICENCE',
     author='mustafakilic',
